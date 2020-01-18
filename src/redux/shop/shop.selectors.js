6e56b685640319ddb8adc38f686ce2982ae17f51
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-01-18 17:16:56 +0530

    container pattern

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -17,3 +17,13 @@ export const selectCollection = colledtionUrlParam =>
   createSelector([selectCollections], collections =>
     collections ? collections[colledtionUrlParam] : null
   );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
+);
